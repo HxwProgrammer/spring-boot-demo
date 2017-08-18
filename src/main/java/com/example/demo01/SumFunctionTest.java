a7195e72ef156558f934ff2da0ba0f551bd7ad49
victor <victor@supernova11.com>
2017-08-18 10:16:00 +0900

    a

---
+++ src/main/java/com/example/demo01/SumFunctionTest.java
@@ -0,0 +1,42 @@
+package com.example.demo01;
+
+import java.util.stream.IntStream;
+
+public class SumFunctionTest {
+	public static void main(String[] args) {
+		System.out.println("sum1ToNumberByForLoop(100):" + sum1ToNumberByForLoop(100));
+		System.out.println("sum1ToNumberByForLoop(99):" + sum1ToNumberByForLoop(99));
+
+		System.out.println("sum1ToNumberByStream(100):" + sum1ToNumberByStream(100));
+		System.out.println("sum1ToNumberByStream(99):" + sum1ToNumberByStream(99));
+
+		System.out.println("sum1ToNumberByRecursiveFunction(100):" + sum1ToNumberByRecursiveFunction(100));
+		System.out.println("sum1ToNumberByRecursiveFunction(99):" + sum1ToNumberByRecursiveFunction(99));
+	}
+
+	public static int sum1ToNumberByForLoop(final int number) {
+		int sum = 0;
+
+		for (int i = 1; i <= number; i++) {
+			sum += i;
+		}
+
+		return sum;
+	}
+
+	public static int sum1ToNumberByStream(final int number) {
+		return IntStream.range(0, number + 1).sum();
+	}
+
+	public static int sum1ToNumberByRecursiveFunction(final int number) {
+		//  ___________________________
+		// |                           |
+		// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+		if (number % 2 == 0) {
+			return (1 + number) * (number / 2);
+		} else {
+			return sum1ToNumberByRecursiveFunction(number - 1) + number;
+		}
+	}
+
+}
