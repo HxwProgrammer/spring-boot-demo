a9c7fa3caa6780f262749eb9d6fd1a137b2bc800
bullcats <windpyj80@gmail.com>
2017-08-24 01:03:24 +0900

    StreamBoardExample 에 Observable 이용하는 것 추가

--- src/main/java/com/example/demo01/StreamBoardExample.java
+++ src/main/java/com/example/demo01/StreamBoardExample.java
@@ -1,5 +1,6 @@
 package com.example.demo01;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -12,9 +13,14 @@ import lombok.Data;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import io.reactivex.Observable;
+
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+/**
+ * 게시글 중 7일 이내 글인 경우 isNew 를 활성화 해주기
+ */
 @Slf4j
 public class StreamBoardExample {
 	public static void main(String[] args) {
@@ -27,39 +33,60 @@ public class StreamBoardExample {
 				new Bbs(4, false, DateTime.now().minusDays(2))
 		);
 
-		System.out.println("Before");
-		System.out.println("================================================");
-		System.out.println(result.stream().map(String::valueOf).collect(joining("\n")));
-
-		// 7일 이내 글인 경우 isNew 를 true 로 설정
+		print("Before", result);
 
 		// 1. Imperative
-//		for (final Bbs bbs : result) {
-//			if (isWithIn7Days(bbs)) {
-//				bbs.setNew(true);
-//			}
-//		}
+		List<Bbs> result1 = byImperative(new ArrayList<>(result));
+		print("After Imperative", result1);
 
 		// 2. Stream
-		final List<Bbs> list = result.stream()
-				.map(bbs -> bbs.setFieldIfTrue(StreamBoardExample::isWithIn7Days, b -> b.setNew(true)))
-				//.peek(bbs -> doIfTrue(isWithIn7Days(bbs), () -> bbs.setNew(true)))
-				.collect(toList());
+		List<Bbs> result2 = byStream(new ArrayList<>(result));
+		print("After Stream", result2);
 
-		System.out.println("\nAfter");
-		System.out.println("================================================");
-		System.out.println(list.stream().map(String::valueOf).collect(joining("\n")));
-
+		// 3. RxJava Observable
+		byRxObservable("After RxJava Observable", new ArrayList<>(result));
 	}
 
-	public static boolean isWithIn7Days(@NonNull Bbs bbs) {
+	private static boolean isWithIn7Days(@NonNull Bbs bbs) {
 		return (bbs.getRegDateTime().isAfter(DateTime.now().minusDays(7).toInstant()));
 	}
 
-	public static void doIfTrue(boolean isTrue, Runnable runnable) {
+	private static void doIfTrue(boolean isTrue, Runnable runnable) {
 		if (isTrue) {
 			runnable.run();
 		}
+	}
+
+	private static void print(final String tag, final List<Bbs> result) {
+		System.out.println(tag);
+		System.out.println("================================================");
+		System.out.println(result.stream().map(String::valueOf).collect(joining("\n")));
+		System.out.println("================================================");
+	}
+
+	private static List<Bbs> byImperative(final List<Bbs> result) {
+		for (final Bbs bbs : result) {
+			if (isWithIn7Days(bbs)) {
+				bbs.setNew(true);
+			}
+		}
+
+		return result;
+	}
+
+	private static List<Bbs> byStream(final List<Bbs> result) {
+		return result.stream()
+				.map(bbs -> bbs.setFieldIfTrue(StreamBoardExample::isWithIn7Days, b -> b.setNew(true)))
+				//.peek(bbs -> doIfTrue(isWithIn7Days(bbs), () -> bbs.setNew(true)))
+				.collect(toList());
+	}
+
+	private static void byRxObservable(final String tag, final List<Bbs> result) {
+		System.out.println(tag);
+		System.out.println("================================================");
+		Observable.fromIterable(result)
+				.map(bbs -> bbs.setFieldIfTrue(StreamBoardExample::isWithIn7Days, b -> b.setNew(true)))
+				.subscribe(System.out::println);
 	}
 
 }
