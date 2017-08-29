4f37438ba36c663fdabe0a3ab1c79552cafe4d6b
victor <victor@supernova11.com>
2017-08-29 14:15:01 +0900

    session list synchronizedList 적용

--- src/main/java/com/example/demo/repository/chat/WebSocketSessionHolder.java
+++ src/main/java/com/example/demo/repository/chat/WebSocketSessionHolder.java
@@ -1,12 +1,13 @@
 package com.example.demo.repository.chat;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.web.socket.WebSocketSession;
 
 public class WebSocketSessionHolder {
-	private static List<WebSocketSession> sessions = new ArrayList<>();
+	private static List<WebSocketSession> sessions = Collections.synchronizedList(new ArrayList<>());
 
 	public static List<WebSocketSession> getSessions() {
 		return sessions;
